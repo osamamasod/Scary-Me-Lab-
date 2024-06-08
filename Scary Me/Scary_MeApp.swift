a61962e9c50fa82fe6f737696124702bd75049cd
Osama Masoud <132358137+osamamasod@users.noreply.github.com>
2024-06-08 18:05:59 +0700

    Initial Commit

---
+++ Scary Me/Scary_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scary_MeApp.swift
+//  Scary Me
+//
+//  Created by Osama Masoud on 6/8/24.
+//
+
+import SwiftUI
+
+@main
+struct Scary_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
